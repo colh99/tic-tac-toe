cffa59693cfde34fdd520c2d227ad32aa5069a8f
colh99 <colbyhale199@gmail.com>
2022-04-30 17:49:42 -0600

    set up

--- Program.cs
+++ Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 
-namespace TicTacToe // Note: actual namespace depends on the project name.
+namespace TicTacToe
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string Player1 = "X";
+            string Player2 = "O";
+            string CurrentPlayer = Player1;
+            List<string> grid = new List<string>();
+            bool StillPlaying = true;
+
+        }
+        static void CheckForWin(List<string> grid)
+        {
+        
+        }
+
+        static void CheckForDraw(List<string> grid)
+        {
+
+        }
+
+        static void CreateGrid(List<string> grid)
+        {
+
+        }
+
+        static void MakeMark(string CurrentPlayer)
+        {
+
+        }
+
+        static void DrawGrid(List<string> grid)
+        {
+            
         }
     }
 }
